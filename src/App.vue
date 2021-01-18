e902a0d3051d73785a0813db4c9472492ed2d5f5
Jaedsada Chaisorn <jaedsada@Jaedsadas-MacBook-Pro.local>
2021-01-18 20:31:15 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <br>
+  <router-link to="/">Home</router-link> | 
+  <router-link to="/about">About</router-link>
+  <router-view></router-view>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
